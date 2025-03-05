b7ea1cdfb1e8511623e978b9a708b2fda7c2493e
tanishqr079 <tanishqratdiya7@gmail.com>
2025-03-05 22:52:28 +0530

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,14 @@
+// Card.java
+class Card {
+	String rank;
+	String suit;
+
+	public Card(String rank, String suit) {
+		this.rank = rank;
+		this.suit = suit;
+	}
+
+	public String toString() {
+		return rank + " of " + suit;
+	}
+}
